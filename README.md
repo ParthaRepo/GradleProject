6ee9fd3d8735ee7b853e657ee6b3d2de0ee09e36
Logakannan <PXL8108@homedepot.com>
2016-10-01 23:30:03 -0400

    Added the project details

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GradleProject
 Project created to understand the gradle build 
+Added few groovy commands that was used in build.gradle file.
---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.Arrays;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by PXL8108 on 10/1/2016.
+ */
+public class Main {
+    public static void main(String args[]) {
+        System.out.println(StringUtils.swapCase(Arrays.toString(args)));
+    }
+}
